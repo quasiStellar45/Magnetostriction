7cabaf9e29e93a732bd55eefcfd2caaeef1b5b70
quasiStellar45 <79328985+quasiStellar45@users.noreply.github.com>
2023-03-02 09:24:56 -0700

    Add files via upload
    Changes to the phase (phi) and the angular position conversion to 0 to 2pi.

--- Isotropic_2D_Magnetostrictive_Response.m
+++ Isotropic_2D_Magnetostrictive_Response.m
@@ -9,20 +9,20 @@ close all
 
 %% Adjustable Parameters
 % Material Properties
-Ms = 4.908e5; % A/m, Saturation magnetization of Ni at 298K
+Ms = 4.908e2; % kA/m, Saturation magnetization of Ni at 298K
 L0 = -34; % ppm, saturation magnetostriction of Ni
-lambda = 1e-6; % adjustable damping parameter
+lambda = 1; % adjustable damping parameter
 
-% Source Properties
-f = 1; % Hz, source frequency
-H0 = 1e4; % A/M, source amplitude
-phi = 0; % radians, source phase - if set to pi/2, the source becomes cos
-
+% Source Properties, H = H0sin(omega*t-phi)
+f = 100; % Hz, source frequency: omega = 2pi*f
+H0 = 50; % kA/M, source amplitude
+phi = 0; % radians, source phase - if set to -pi/2, the source becomes cos
+         
 % Initial Conditions and FFT Parameters
-theta0 = pi/4; % radians, initial theta position
+theta0 = pi/3; % radians, initial theta position
 t_0 = 0; % s, time where theta and strain plots begin
-t_f = 120; % s, final time
-fs = 1e5; % Hz, FFT sampling frequency
+t_f = 1; % s, final time
+fs = 1e6; % Hz, FFT sampling frequency
 
 %% Differential Equation Solver
 g = 2; % spectroscopic splitting factor for e- spin
@@ -35,9 +35,10 @@ alpha = lambda/(gamma*Ms); % damping constant
 omega = 2*pi*f; % Radians/s source frequency
 tspan = 0:1/fs:t_f-1/fs; % s, time span of interest
 
-H = H0*sin(omega*tspan+phi); % magnetic field signal
+H = H0*sin(omega*tspan-phi); % magnetic field signal
 
-[t,theta] = ode45(@(t,theta) fun(t,theta,gamma,alpha,H0,omega,phi),tspan,theta0);
+[t,theta] = ...
+    ode45(@(t,theta) fun(t,theta,gamma,alpha,H0,omega,phi),tspan,theta0);
 
 %% Plot of theta
 figure(1)
@@ -49,26 +50,21 @@ xlim([t_0 t_f])
 yyaxis right
 plot(t,H,'r')
 ylim([-10*H0 10*H0])
-ylabel('H (A/m)')
+ylabel('H (kA/m)')
 legend('\theta','H')
 
 %% Converted plot of theta to radians from 0 to 2pi
 conv_sin = sin(theta); % taking sin of theta for conversion 
 conv_cos = cos(theta); % taking cosin of theta for conversion
 
-% Converting the angles to values from -pi to pi
 theta_rad = zeros(size(theta));
-for i = 1:length(t)
-    if (conv_sin(i) >= 0) && (conv_cos(i) > 0)
+for i = 1:length(theta_rad)
+    if conv_sin(i) >= 0
         theta_rad(i) = acos(conv_cos(i));
-    elseif (conv_sin(i) > 0) && (conv_cos(i) <= 0)
-        theta_rad(i) = asin(conv_sin(i))+pi/2;
-    elseif (conv_sin(i) <= 0) && (conv_cos(i) < 0)
-        theta_rad(i) = acos(conv_cos(i))-3*pi/2;
-    elseif (conv_sin(i) < 0) && (conv_cos(i) >= 0)
-        theta_rad(i) = asin(conv_sin(i));
-    else
-        print('error')
+    elseif (conv_sin(i) < 0) && (conv_cos(i) <= 0)
+        theta_rad(i) = pi + acos(-conv_cos(i));
+    elseif (conv_sin(i) < 0) && (conv_cos(i) > 0)
+        theta_rad(i) = 2*pi + asin(conv_sin(i));
     end
 end
 
@@ -83,7 +79,7 @@ plot(t,H,'r')
 hold on
 plot(t,zeros(length(t),1))
 ylim([-H0 10*H0])
-ylabel('H (A/m)')
+ylabel('H (kA/m)')
 legend('\theta','H')
 hold off
 
@@ -93,14 +89,14 @@ ll = 3/2*L0.*(cos(theta).^2-1/3); % ppm, homogeneous strain response
 figure(3)
 plot(t,ll,'-')
 xlabel('Time (s)');
-ylabel('\lambda_{\theta} (ppm)');
+ylabel('\Lambda_{\theta} (ppm)');
 title('Strain Response')
 xlim([t_0 t_f])
 yyaxis right
 plot(t,H,'r')
 ylim([-10*H0 10*H0])
-ylabel('H (A/m)')
-legend('\theta','H')
+ylabel('H (kA/m)')
+legend('\Lambda','H')
 
 %% Fourier Transform
 y = fft(ll);
@@ -113,9 +109,9 @@ xlabel('Frequency (Hz)')
 ylabel('Magnitude')
 title('Fourier Transform')
 ylim([0 5])
-xlim([0 100*f])
+xlim([0 50*f])
 
 
 function dthetadt = fun(t,theta,gamma,alpha,H0,omega,phi)
-dthetadt = gamma*(1+alpha^2)/(1+alpha)*H0.*sin(omega.*t+phi).*sin(theta);
+dthetadt = gamma*(1+alpha^2)/(1+alpha)*H0.*sin(omega.*t-phi).*sin(theta);
 end
